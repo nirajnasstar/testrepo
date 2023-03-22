e71ecd9e71d7669f866544ae51b5271120064b64
nirajnasstar <128639150+nirajnasstar@users.noreply.github.com>
2023-03-22 21:05:05 +0000

    Update scriptps.ps1

--- scriptps.ps1
+++ scriptps.ps1
@@ -1,13 +1,13 @@
-$budgetExists = 'No' `
-$budget = Get-AzConsumptionBudget -ResourceGroupName td-niraj-ps -Name 'DefaultBudget' -ErrorAction SilentlyContinue `
-if ($budget) {  $budgetExists = 'Yes' } `
+$budgetExists = 'No'
+$budget = Get-AzConsumptionBudget -ResourceGroupName td-niraj-ps -Name 'DefaultBudget' -ErrorAction SilentlyContinue
+if ($budget) {  $budgetExists = 'Yes' } 
 else { `
 	New-AzResourceGroupDeployment `
    -Name ExampleDeployment `
   -ResourceGroupName td-niraj-ps `
-  -TemplateFile "https://raw.githubusercontent.com/nirajnasstar/testrepo/main/budgettemplate.json"  
+  -TemplateFile "https://raw.githubusercontent.com/nirajnasstar/testrepo/main/budgettemplate.json" `  
 } `
-Write-Output $budgetExists ` 
-$DeploymentScriptOutputs = @{} ` 
+Write-Output $budgetExists 
+$DeploymentScriptOutputs = @{} 
 $DeploymentScriptOutputs['text'] = $budgetExists
 
